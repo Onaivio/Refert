bf90a9395f48ffa075f682322113649e5fa38097
Onaivio <onaivio220@gmail.com>
2024-09-05 12:22:08 +0100

    feat: Add siteMetadata file with app information

---
+++ src/utils/siteMetadata.ts
@@ -0,0 +1,19 @@
+const siteMetadata = {
+    title: 'Refert',
+    author: 'Okomayin Onaivi',
+    headerTitle: 'Refert App',
+    description: 'A simple app created with React.js and Tailwind.css with Infinite Scrolling',
+    language: 'en-us',
+    theme: 'system',
+    siteUrl: 'https://refert.vercel.app/',
+    siteRepo: 'https://github.com/onaivio/refert',
+    siteLogo: `/logo.png`,
+    email: 'onaivio220@gmail.com',
+    github: 'https://github.com/onaivio',
+    twitter: 'https://twitter.com/OkomayinOnaivi',
+    facebook: 'https://facebook.com/onaivio',
+    instagram: 'https://www.instagram.com/theonaivio',
+    locale: 'en-US',
+}
+
+export default siteMetadata;
